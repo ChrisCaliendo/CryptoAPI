97167aaa2943c6f9f2e1de5013c03ec6fb773c23
Christian Caliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2024-05-14 13:34:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -17,31 +17,38 @@ app.listen(port, () => {
 })
 
 app.get("/", async(req, res) => {
-    if (cache && cache.data) return res.send(cache.data);
-    try {
-        //https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest
-        const url = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=500`;
-        const options = {
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CMC_PRO_API_KEY': process.env.API_KEY
+    if (cache.length > 0) {
+        return res.send(cache);
+    }
+    else{
+        try {
+            //https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest
+            const url = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=500`;
+            const options = {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CMC_PRO_API_KEY': process.env.API_KEY
+                }
             }
+            const response = await fetch(url, options);
+            const data = await response.json();
+            const subset = data.data.map(({ id, name, quote, max_supply, circulating_supply}) => ({ 
+                id: id, 
+                name: name,
+                value: quote.USD.price,
+                maxSupply: max_supply,
+                circulation: circulating_supply,
+                marketCap: quote.USD.market_cap,
+                percentChange: quote.USD.percent_change_24h
+            }));
+            res.send(subset);
+            cache = subset;
+            console.log(subset);
         }
-        const response = await fetch(url, options);
-        const data = await response.json();
-        const subset = data.data.map(({ id, name, quote, max_supply, circulating_supply}) => ({ 
-            id: id, 
-            name: name,
-            value: quote.USD.price,
-            maxSupply: max_supply,
-            circulation: circulating_supply,
-            marketCap: quote.USD.market_cap,
-        }));
-        res.send(subset);
-        cache = subset;
+        catch (err) {
+            console.log(err);
+            res.status(500).send("Something went wrong");
+        }
     }
-    catch (err) {
-        console.log(err);
-        res.status(500).send("Something went wrong");
-    }
+    
 })
